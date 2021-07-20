b05d12576509a47955c7635249e515237c44b5db
Pavel George Iulian <gpavel@Pavels-MacBook-Air.local>
2021-07-20 17:36:45 +0200

    one thing, conquest the galaxy

---
+++ ft_precision_p.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_precision_p.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gpavel <gpavel@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/20 14:55:40 by gpavel            #+#    #+#             */
+/*   Updated: 2021/07/20 17:30:49 by gpavel           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void		ft_printf_precision_p(va_list ap, int *cont, struct mystruct var)
+{
+	char	*str;
+	int		strlen;
+
+	str = ft_printf_p(ap, cont);
+	strlen = ft_strlen(str);
+	while (var.p1-- > strlen)
+	{
+		ft_putchar_fd(" ", 0);
+		cont++;
+	}
+	while (str--)
+	{
+		ft_putchar_fd(*str, 0);
+		cont++;
+	}
+	return (0);
+}
